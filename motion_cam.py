426d018840f7a0a9aa0282a6ca53c13e76a9df62
willimach <willi@mach.at>
2016-12-28 12:58:37 +0100

    added threading, added video mode = faster, added datestamp in pic, cleand up

--- motion_cam.py
+++ motion_cam.py
@@ -3,21 +3,13 @@ from motion_cam_lib import *
 import motion_conf
 from threading import Thread, active_count
 
-difference = motion_conf.difference
-pixels     = motion_conf.pixels
-
-max_uploads = motion_conf.max_uploads
-uploads     = motion_conf.uploads
-
-pic_compare_width  = motion_conf.pic_compare_width
-pic_compare_height = motion_conf.pic_compare_height
-
-
 camera = picamera.PiCamera()
 camera.rotation = 0
 # sets black/white pictures
 camera.color_effects= (128,128)
 camera.resolution = (motion_conf.pic_compare_width, motion_conf.pic_compare_height)
+
+
 
 # start main code:
 buffer1 = compare(camera)
@@ -27,13 +19,13 @@ while (True):
    
    buffer2 = compare(camera) #takes 0.5s
    changedpixels = 0
-   for x in range(0, pic_compare_width): #takes 0.1s
-      for y in range(0, pic_compare_height):
+   for x in range(0, motion_conf.pic_compare_width): #takes 0.1s
+      for y in range(0, motion_conf.pic_compare_height):
          pixdiff = abs(buffer1[x,y][1] - buffer2[x,y][1])
-         if pixdiff > difference:
+         if pixdiff > motion_conf.difference:
             changedpixels += 1
    mytime=round(time.time())
-   if changedpixels > pixels:
+   if changedpixels > motion_conf.pixels:
       #makes picture and uploads it
       #filename = newimage(changedpixels,camera)
       t = Thread(target=myftp, args=(newimage(changedpixels,camera),))
